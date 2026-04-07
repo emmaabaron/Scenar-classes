45cf7f11de4d316c29e08ddfbb54b60dc9f1fe5b
BARON Emma <BBARON@SIO.LOCAL>
2026-04-07 15:23:28 +0200

    mission 5

--- src/scenar/classes/ScenarClasses.java
+++ src/scenar/classes/ScenarClasses.java
@@ -21,7 +21,7 @@ public class ScenarClasses {
         int annee = 2021;
         int prix = 14500;
         System.out.println("Voici une rapide présentation de la "+marque + modele +" sortie en "+annee + ". Elle est vendu au prix de "+prix+ "euros.");
-        */
+        
         Voiture v1 = new Voiture();
         Voiture v2 = new Voiture();
         Voiture v3 =new Voiture();
@@ -29,6 +29,10 @@ public class ScenarClasses {
         v1.marque="Peugeot"; v1.modele="208"; v1.annee=2021; v1.prix=14500;
         v2.marque="Toyota"; v2.modele="Yaris"; v2.annee=2015; v2.prix=9800;
         v3.marque="Renault"; v3.modele="Clio"; v3.annee=2000; v3.prix=3500;
+*/
+        Voiture v1 = new Voiture("Peugeot","208",2021,14500);
+        
+        System.out.println(v1.marque+ v1.modele);
     }
     
 }
